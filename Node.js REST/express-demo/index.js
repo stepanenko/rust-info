80a0167e67cd5470c5fd2d8a5109e7d5ae036bfd
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-09-24 17:19:31 +0300

    mosh

--- Node.js REST/express-demo/index.js
+++ Node.js REST/express-demo/index.js
@@ -1,15 +1,52 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: 'course1'},
+  { id: 2, name: 'course2'},
+  { id: 3, name: 'course3'},
+]
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!')
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1,2,3,4,5]);
-})
-// app.post();
-// app.put();
-// app.delete();
+  res.send(courses);
+});
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course was not found');
+  res.send(course);
+});
 
-app.listen(3000, () => console.log('Listening on port 3000...'))
+app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required();
+  }
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send('The name is required and should be more than 3 chars');
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+})
+// app.get('/api/posts/:year/:month', (req, res) => {
+//   res.send(req.params) // to get parameters like '../posts/2018/1'
+// })
+// app.get('/api/posts/:year/:month', (req, res) => {
+//   res.send(req.query) // to get query like '?sortBy=name'
+// })
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+//console.log(app);
