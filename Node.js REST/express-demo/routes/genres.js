b54fb814438425823a6176bd09455d7131bb82f3
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-09-27 22:27:33 +0300

    genres finished

--- Node.js REST/express-demo/routes/genres.js
+++ Node.js REST/express-demo/routes/genres.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const genres = [
   { id: 1, genre: "horror"},
@@ -12,18 +13,39 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  let genre = {};
-  genre.id = genres.length + 1;
-  genre.genre = req.body.genre;
-  genres.push(genre);
-  res.send(genre);
+  let gen = {};
+  gen.id = genres.length + 1;
+  gen.genre = req.body.genre;
+  genres.push(gen);
+  res.send(gen);
+});
+
+router.put('/:id', (request, response) => {
+  const gen = genres.find(g => g.id == request.params.id);
+  if (!gen) return response.status(404).send('Such genre was not found');
+
+  const { error } = validateGenre(request.body);
+  if (error) return response.status(400).send(error.details[0].message);
+
+  gen.genre = request.body.genre;
+  response.send(`Genre with id ${request.params.id} was updated`);
 });
 
 router.delete('/:id', (req, res) => {
-  let id = req.params.id;
-  console.log(id)
-  //let deleted = genres.slice(, 1);
-  //res.send(deleted);
-})
+  const gen = genres.find(g => g.id == req.params.id);
+  if (!gen) return res.status(404).send("Such genre was not found");
+
+  let index = genres.indexOf(gen);
+  genres.splice(index, 1);
+  res.send(`Genre with index ${index} was deleted`);
+});
+
+function validateGenre(genre) {
+  const schema = {
+    genre: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
 
 module.exports = router;
