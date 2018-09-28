94172240e6e54a2fc01bced672df2ba71b47c976
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-09-28 20:22:46 +0300

    async code

--- Node.js REST/async-demo/index.js
+++ Node.js REST/async-demo/index.js
@@ -1,5 +1,17 @@
 console.log('before');
-setTimeout(() => {
-  console.log('timeout')
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log('after');
+
+//callbacks
+//Promises
+//async/await
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log('Reading a user from database...');
+    return { id: id, githubname: 'sergio'};
+  }, 2000);
+
+  return 1;
+}
