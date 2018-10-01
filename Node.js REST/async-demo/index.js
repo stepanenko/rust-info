dc3e45b0fd5a4e8d6f0684e7e90f8558cb4acfde
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-10-01 15:24:49 +0300

    named functions rescue

--- Node.js REST/async-demo/index.js
+++ Node.js REST/async-demo/index.js
@@ -1,17 +1,36 @@
 console.log('before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+  console.log('User: ', user);
+  
+  getRepos(user.githubname, (repos) => {
+    console.log(`${user.githubname}'s repos: ${repos}.`)
+
+    getCommits(repos[0], (commits) => {
+      console.log(`List of ${repos[0]}'s commits: ${commits}`)
+    })
+  });
+
+});
 console.log('after');
 
-//callbacks
-//Promises
-//async/await
+// Named Function to rescue, look in namedFunctions.js...
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from database...');
-    return { id: id, githubname: 'sergio'};
-  }, 2000);
+    callback({ id: id, githubname: 'Sergio'});
+  }, 1000);
+}
 
-  return 1;
+function getRepos(username, callback) {
+  setTimeout(() => {
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 1000) 
+}
+
+function getCommits(repoName, callback) {
+  setTimeout(() => {
+    console.log(`Looking for commits in ${repoName}...`);
+    callback(['commit1', 'commit2'])
+  }, 1000)
 }
