a9e59e897c41c15b59d57b3ac467652f746a2a51
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-21 00:45:46 +0300

    benchmarking

--- 02_performance/index_simple.js
+++ 02_performance/index_simple.js
@@ -1,28 +1,29 @@
 
-// process.env.UV_THREADPOOL_SIZE = 1;   // default is 4 threads
-
 const cluster = require('cluster');
-const crypto = require('crypto');
 const express = require('express');
 const app = express();
 
 if (cluster.isMaster) {
   // Cause index.js to be executed 'again' but in a child mode
   cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 } else {
   // Im a child, Im going to act like a server and do nothing else
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {} // simulates long operation
+  }
+  
   app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hi there');
-    });
+    doWork(5000);
+    res.send('Hi there');
   });
 
   app.get('/fast', (req, res) => {
     res.send('That was fast!');
   });
-
+  
   app.listen(3000, () => console.log('server is on...'));
 }
---
+++ 02_performance/readme.md
@@ -0,0 +1,8 @@
+
+## Using Apache Benchmarking:
+
+In Terminal run `ab -c 50 -n 500 localhost:3000` (or whatever your url is)
+
+- `-c` concurrency  Number of multiple requests to make at a time
+- `-n` requests     Number of requests to perfor
+- `ab` to list all options...
